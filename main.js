3dfd163ec114b16330d5b58c89f11b09c619236b
Edmond <edmondyang11@gmail.com>
2021-09-05 18:38:35 -0400

    changed file path

--- main.js
+++ main.js
@@ -10,64 +10,64 @@ const progressContainer = document.querySelector('.progress-container')
 //initialize song list by nesting objects 
  const songList = [
     {
-        path: "../assets/songs/and-so-it-begins.mp3",
+        path: "assets/songs/and-so-it-begins.mp3",
         songName: "And So It Begins",
         artist: "Artificial.Music",
-        image: "../assets/img/1.png"
+        image: "assets/img/1.png"
     },
     {
-        path: "../assets/songs/bedtime-after-a-coffee.mp3",
+        path: "assets/songs/bedtime-after-a-coffee.mp3",
         songName: "Bedtime After a Coffee",
         artist: "Barradeen",
-        image: "../assets/img/2.png"
+        image: "assets/img/2.png"
     },
     {
-        path: "../assets/songs/coral.mp3",
+        path: "assets/songs/coral.mp3",
         songName: "Coral",
         artist: "LiQWYD",
-        image: "../assets/img/3.png"
+        image: "assets/img/3.png"
     },
     {
-        path: "../assets/songs/dreams-come-true.mp3",
+        path: "assets/songs/dreams-come-true.mp3",
         songName: "Dreams Come True",
         artist: "Purrple Cat",
-        image: "../assets/img/4.png"
+        image: "assets/img/4.png"
     },
     {
-        path: "../assets/songs/equinox.mp3",
+        path: "assets/songs/equinox.mp3",
         songName: "Equinox",
         artist: "Purrple Cat",
-        image: "../assets/img/5.png"
+        image: "assets/img/5.png"
     },
     {
-        path: "../assets/songs/fragile.mp3",
+        path: "assets/songs/fragile.mp3",
         songName: "Fragile",
         artist: "Keys of Moon",
-        image: "../assets/img/6.jpg"
+        image: "assets/img/6.jpg"
     },
     {
-        path: "../assets/songs/herbal-tea.mp3",
+        path: "assets/songs/herbal-tea.mp3",
         songName: "Herbal Tea",
         artist: "Artificial.Music",
-        image: "../assets/img/7.png"
+        image: "assets/img/7.png"
     },
     {
-        path: "../assets/songs/japan.mp3",
+        path: "assets/songs/japan.mp3",
         songName: "Japan",
         artist: "Uniq",
-        image: "../assets/img/8.png"
+        image: "assets/img/8.png"
     },
     {
-        path: "../assets/songs/water-wood-and-stone.mp3",
+        path: "assets/songs/water-wood-and-stone.mp3",
         songName: "Water Wood and Stone",
         artist: "Audionautix",
-        image: "../assets/img/9.png"
+        image: "assets/img/9.png"
     },
     {
-        path: "../assets/songs/you-know-why.mp3",
+        path: "assets/songs/you-know-why.mp3",
         songName: "You Know Why",
         artist: "Loyalty Freak Music",
-        image: "../assets/img/10.jfif"
+        image: "assets/img/10.jfif"
     }
  ];
 
@@ -136,6 +136,8 @@ function nextSong() {
         playSong();
     }
 }
+
+//update progress on click
 function setProgress(e) {
     const width = this.clientWidth;
     const clickX = e.offsetX;
@@ -143,6 +145,8 @@ function setProgress(e) {
   
     audio.currentTime = (clickX / width) * duration;
   }
+
+//update progress bar based on song length
 function updateProgress(e) {
     const { duration, currentTime } = e.srcElement;
     const progressPercent = (currentTime / duration) * 100;
@@ -150,6 +154,8 @@ function updateProgress(e) {
   }
 loadSong(songList[i]);
 
+
+//event listeners
 next.addEventListener("click", nextSong);   
 prev.addEventListener("click", prevSong);   
 
